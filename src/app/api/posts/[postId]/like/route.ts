13f2b2feaf394663a7d7af98eadfc20220f74c03
RUDRANSHFLY <mayamayavir@gmail.com>
2024-09-28 11:19:15 +0530

    Add API routes for fetching and liking posts

---
+++ src/app/api/posts/[postId]/like/route.ts
@@ -0,0 +1,60 @@
+import { connectDB } from "../../../../../../db/db";
+import { NextResponse } from "next/server";
+import { Post } from "../../../../../../model/post";
+import { auth, currentUser } from "@clerk/nextjs/server";
+
+export async function GET(
+  request: Request,
+  { params }: { params: { postId: string } }
+) {
+  try {
+    await connectDB();
+    const post = await Post.findById(params.postId);
+
+    if (!post) {
+      return NextResponse.json({ error: "Post not found" }, { status: 404 });
+    }
+
+    const likes = post.likes;
+
+    return NextResponse.json(likes);
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error: `Error occured while liking post: ${error}`,
+      },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(
+  request: Request,
+  { params }: { params: { postId: string } }
+) {
+  auth().protect();
+
+  await connectDB();
+
+  const user = await currentUser();
+
+  try {
+    const post = await Post.findById(params.postId);
+
+    if (!post) {
+      return NextResponse.json({ error: "Post not found" }, { status: 404 });
+    }
+    if (user) {
+      await post.likePost(user?.id);
+    }
+
+    return NextResponse.json({ message: "Post Liked Sucessfully" });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error: `Error occured while deleting post: ${error}`,
+      },
+      { status: 500 }
+    );
+  }
+}
