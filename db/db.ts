0b472e4336ffbf0f5132cde159fc954380ce9725
RUDRANSHFLY <mayamayavir@gmail.com>
2024-09-28 11:14:20 +0530

    Add database connection logic

---
+++ db/db.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const connectionString = process.env.MONGO_DB_URL;
+
+if (!connectionString) {
+  throw new Error("Please provide a valid connection string");
+}
+
+export const connectDB = async () => {
+  if (mongoose.connection?.readyState >= 1) {
+    console.log("---Already connected to MongoDB---");
+    return;
+  }
+
+  try {
+    console.log("---Connecting to MongoDB---");
+  } catch (error) {
+    console.log("---Error connected to MongoDB---", error);
+  }
+};
