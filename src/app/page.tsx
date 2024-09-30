5cc2bed1882f26e4dbdaabc9b2a318d9643519c6
RUDRANSHFLY <mayamayavir@gmail.com>
2024-09-30 17:42:33 +0530

    Refactor page component to connect to the database and display post feed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import PostFeed from "@/components/post/PostFeed";
 import PostForm from "@/components/post/PostForm";
 import UserInfo from "@/components/UserInfo";
+import { connectDB } from "@/db/db";
+import { Post } from "@/model/post";
 
-export default function Home() {
+export const revalidate = 0;
+
+export default async function Home() {
+  await connectDB();
+  const posts = await Post.getAllPosts();
+
   return (
     <div className={"grid max-w-6xl mx-auto grid-cols-8 mt-5 sm:px-5"}>
       <section className={"hidden md:inline md:col-span-2"}>
@@ -16,6 +24,7 @@ export default function Home() {
         {/* post form */}
         <PostForm />
         {/* post feed */}
+        <PostFeed posts={posts} />
       </section>
       <section className={"hidden xl:inline justify-center col-span-2"}>
         {/* user profile widegt 2 */}
