21ec61898e017173fd79c2d38cf80cae4e6a9246
RUDRANSHFLY <mayamayavir@gmail.com>
2024-09-30 17:43:09 +0530

    Refactor Comment model and schema

---
+++ src/model/cooment.ts
@@ -0,0 +1,31 @@
+import mongoose, { Schema, Document, models } from "mongoose";
+import { IUser } from "../../types/user";
+
+export interface ICommentBase {
+  user: IUser;
+  text: string;
+}
+
+export interface IComment extends Document, ICommentBase {
+  _id: mongoose.Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const commentSchema = new Schema<IComment>(
+  {
+    user: {
+      userId: { type: String, required: true },
+      userImage: { type: String, required: true },
+      firstName: { type: String, required: true },
+      lastName: { type: String, required: false },
+    },
+    text: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Comment =
+  models.Comment || mongoose.model<IComment>("Comment", commentSchema);
