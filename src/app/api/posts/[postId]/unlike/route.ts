e4b7e559e1cad2180090f87c5318335bc85e847f
RUDRANSHFLY <mayamayavir@gmail.com>
2024-09-30 17:51:53 +0530

    Refactor unlike route for posts API

---
+++ src/app/api/posts/[postId]/unlike/route.ts
@@ -0,0 +1,36 @@
+import { auth, currentUser } from "@clerk/nextjs/server";
+import { connectDB } from "../../../../../../db/db";
+import { NextResponse } from "next/server";
+import { Post } from "../../../../../../model/post";
+
+export async function POST(
+  request: Request,
+  { params }: { params: { postId: string } }
+) {
+  auth().protect();
+
+  await connectDB();
+
+  const user = await currentUser();
+
+  try {
+    const post = await Post.findById(params.postId);
+
+    if (!post) {
+      return NextResponse.json({ error: "Post not found" }, { status: 404 });
+    }
+
+    if (user) {
+      await post.unlikePost(user?.id);
+    }
+
+    return NextResponse.json({ messgae: "post un-linked succesfully" });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error: `Error occured while un-like ing it post: ${error}`,
+      },
+      { status: 500 }
+    );
+  }
+}
