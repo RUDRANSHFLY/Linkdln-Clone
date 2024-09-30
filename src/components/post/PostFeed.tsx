280847c87aa0832b10a6f63401b4e83e56284e0d
RUDRANSHFLY <mayamayavir@gmail.com>
2024-09-30 17:44:24 +0530

    Refactor PostFeed component to display a feed of posts

---
+++ src/components/post/PostFeed.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import { IPostDocument } from "@/model/post";
+import Post from "./Post";
+
+interface PostFeedProps {
+  posts: IPostDocument[];
+}
+
+const PostFeed = ({ posts }: PostFeedProps) => {
+  return (
+    <div className={"space-y-2 pb-20"}>
+      {posts.map((post) => (
+        <Post post={post} key={post.id} />
+      ))}
+    </div>
+  );
+};
+
+export default PostFeed;
