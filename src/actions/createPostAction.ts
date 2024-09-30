29c9dff2ed9b5af2c3752ad6da38025f3a165789
RUDRANSHFLY <mayamayavir@gmail.com>
2024-09-30 17:41:40 +0530

    Refactor createPostAction to upload image and create post with image or text

--- src/actions/createPostAction.ts
+++ src/actions/createPostAction.ts
@@ -3,7 +3,10 @@
 
 import { currentUser } from "@clerk/nextjs/server";
 import { IUser } from "../../types/user";
-import { Post } from "../../model/post";
+import { Post } from "../model/post";
+import { connectDB } from "../db/db";
+import axios from "axios";
+import { revalidatePath } from "next/cache";
 
 export default async function createPostAction(formData: FormData) {
   const user = await currentUser();
@@ -14,7 +17,7 @@ export default async function createPostAction(formData: FormData) {
 
   const postInput = formData.get("postInput") as string;
   const image = formData.get("image") as File;
-  // let imageUrl: string | undefined;
+  let imageUrl: string | undefined;
 
   if (!postInput) {
     throw new Error("post input is required");
@@ -27,10 +30,43 @@ export default async function createPostAction(formData: FormData) {
     lastName: user.lastName || "Dev",
   };
 
+  await connectDB();
+
   try {
     // upload image if it have
     if (image.size > 0) {
       // ! create post with image and text
+
+      const form = new FormData();
+      form.append("file", image);
+      form.append("fileName", image.name);
+      form.append("publicKey", "public_mcKe/WsMmnhYVA8AjRyFIHX79Bk=");
+
+      const options = {
+        method: "POST",
+        url: "https://upload.imagekit.io/api/v2/files/upload",
+        headers: {
+          "Content-Type":
+            "multipart/form-data; boundary=---011000010111000001101001",
+          Accept: "application/json",
+          Authorization:
+            "Basic cHJpdmF0ZV9tS0IzbTFiYmdTZE1IR3JkQkZvOWd6aXJxdWM9Og==",
+        },
+        data: form,
+      };
+
+      try {
+        const { data } = await axios.request(options);
+        imageUrl = data.url;
+        const body = {
+          user: userDB,
+          text: postInput,
+          imageUrl: imageUrl,
+        };
+        await Post.create(body);
+      } catch (error) {
+        console.error(error);
+      }
     } else {
       // ! create post without image with only text
       const body = {
@@ -42,4 +78,6 @@ export default async function createPostAction(formData: FormData) {
   } catch (error) {
     throw new Error(`Failed to create a post: ${error}`);
   }
+
+  revalidatePath("/");
 }
